39b4a418fb9a4af3824ec1e6375d85a95bfbcde0
t_wheeler <twh270@gmail.com>
2013-10-26 07:01:04 -0500

    encode 1

--- src/test/java/org/byteworks/cbor/impl/WhenEncodingPositiveIntegers.java
+++ src/test/java/org/byteworks/cbor/impl/WhenEncodingPositiveIntegers.java
@@ -5,18 +5,39 @@ import static org.junit.Assert.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class WhenEncodingPositiveIntegers {
+  private ByteArrayOutputStream bos;
+  private CBOROutputStream os;
+
+  @Before
+  public void beforeTest() {
+    bos = new ByteArrayOutputStream();
+    os = new CBOROutputStream(bos);
+  }
+  
   @Test
   public void encodesZero() throws IOException {
-    ByteArrayOutputStream bos = new ByteArrayOutputStream();
-    CBOROutputStream os = new CBOROutputStream(bos);
     int b = 0;
     os.write(b);
+    assertWritten(1, 0);
+  }
+
+  @Test
+  public void encodesOne() throws IOException {
+    int b = 1;
+    os.write(b);
+    assertWritten(1, 1);
+  }
+
+  private void assertWritten(int length, int...expectedBytes) throws IOException {
     os.close();
     byte[] bytes = bos.toByteArray();
-    assertEquals(1, bytes.length);
-    assertEquals(0, bytes[0]);
+    assertEquals(length, bytes.length);
+    for(int i = 0; i < expectedBytes.length; i++) {
+      assertEquals(expectedBytes[i], bytes[i]);
+    }
   }
 }
